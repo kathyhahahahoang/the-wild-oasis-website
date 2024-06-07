ed3ce0ecb1afc70eac366787439f44647577fd46
Kathy Hoang <kathyhahoang@gmail.com>
2024-06-07 15:35:53 -0500

    Add revalidation to cabins page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,10 @@ import CabinList from "@/app/_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "../_components/Spinner";
 
+// export const revalidate = 0;
+// export const revalidate = 15;
+export const revalidate = 3600;
+
 export const metadata = {
   title: "Cabins",
 };
