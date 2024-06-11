f67c7d337fa5c2c94f4ca27cdf5029be83215c62
Kathy Hoang <kathyhahoang@gmail.com>
2024-06-11 10:49:25 -0500

    Highlight active link in SideNavigation

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -34,16 +34,13 @@ export default async function Page({ params }) {
             alt={`Cabin ${name}`}
           />
         </div>
-
         <div>
           <h3 className="text-accent-100 font-black text-7xl mb-5 translate-x-[-254px] bg-primary-950 p-6 pb-1 w-[150%]">
             Cabin {name}
           </h3>
-
           <p className="text-lg text-primary-300 mb-10">
             <TextExpander>{description}</TextExpander>
           </p>
-
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
               <UsersIcon className="h-5 w-5 text-primary-600" />
