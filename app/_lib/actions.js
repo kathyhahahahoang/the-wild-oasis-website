e07e631549646095b442fc895695958851c48257
Kathy Hoang <kathyhahoang@gmail.com>
2024-06-21 15:06:21 -0500

    Create deleteReservation server action

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -26,6 +26,28 @@ export async function updateGuest(formData) {
   revalidatePath("/account/profile");
 }
 
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingsIds.includes(bookingId)) {
+    throw new Error("You are not allowed to delete this booking");
+  }
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) {
+    throw new Error("Booking could not be deleted");
+  }
+  revalidatePath("/account/reservations");
+}
+
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
 }
